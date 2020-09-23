093e187fea8433d6260e7f1d5c995e30afbc50be
yungjin702 <53703022+yungjin702@users.noreply.github.com>
2020-09-23 10:42:45 +0900

    Add files via upload

---
+++ Scripts/first.js
@@ -0,0 +1,8 @@
+/* eslint no-unused-vars: "error"*/
+/* eslint-disable no-unused-vars */
+/* eslint-env browser */
+
+function viewCurrDate() { //eslint-disable-lineno-unused-vars
+    "use strict";
+    document.getElementById('demo').innerHTML = Date();
+}
