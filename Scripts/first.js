8be27671a71e6bfe75e45e8a1a4b1a34c55232c7
yungjin702 <53703022+yungjin702@users.noreply.github.com>
2020-09-23 11:42:56 +0900

    Add files via upload

--- Scripts/first.js
+++ Scripts/first.js
@@ -5,4 +5,15 @@
 function viewCurrDate() { //eslint-disable-lineno-unused-vars
     "use strict";
     document.getElementById('demo').innerHTML = Date();
+    //alert("안녕하세요");
+    var name = prompt("이름을 입력하세요");
+    alert("입력한 이름은" + name);
+    
+    var ret = confirm("다시 입력하겠습니까?");
+    alert(ret);
+}
+
+function changeColor()
+{
+    document.getElementById('test').style.color = "red";
 }
